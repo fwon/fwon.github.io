383384072b90f3cf5dc9de2fea915f2b122b3ac0
wangfengmmd <feng@graymatics.com>
2016-01-21 14:52:16 +0800

    minify

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -3,9 +3,9 @@ var webpack = require('webpack')
 var px2rem = require('postcss-px2rem')
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  // devtool: 'cheap-module-eval-source-map',
   entry: [
-    'webpack-hot-middleware/client',
+    // 'webpack-hot-middleware/client',
     './index'
   ],
   output: {
